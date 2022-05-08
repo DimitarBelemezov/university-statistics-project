f35b65d925e02179765aeadfdeffa93a413c92bb
Dima <dima.belemezov@gmail.com>
2022-05-08 09:19:43 +0300

    WI 001 : Tendency and PearsonCoef

---
+++ src/PTS_Project.java
@@ -0,0 +1,8 @@
+public class PTS_Project
+{
+    public static void main(String[] args) {
+        Tendency tendency = new Tendency();
+        tendency.tendencyCalc();
+        tendency.getPearsonCoef();
+    }
+}
